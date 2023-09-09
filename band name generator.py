4a7e8bdf51fafca235999bf533cb5ba7d890f257
Sripurna Maitra Biswas <76967321+sripurna532@users.noreply.github.com>
2023-09-09 13:06:54 +0530

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,9 @@
+# print makes the console , print the value
+# input takes input from user
+print("Hello" + " Sri")
+name = input("What is your name?")
+print(name)
+# variable used to store data
+city = input("What's the city you grew up in?\n")
+pet_name = input("What's your pet's name\n")
+print("Your band name is " + city + " " + pet_name)
